b7f518e71f9bb21289e9bbc6ecb00c8c9d8f42d1
Sergii Golyshev <cyber.tchief@gmail.com>
2022-08-16 22:39:23 +0300

    feat: add forwardToUser

--- mod.ts
+++ mod.ts
@@ -1,19 +1,29 @@
 import { Bot, Context, webhookCallback } from 'https://deno.land/x/grammy@v1.10.1/mod.ts';
-import { User } from "https://deno.land/x/grammy@v1.10.1/types.deno.ts";
+import { User, Message } from "https://deno.land/x/grammy@v1.10.1/types.deno.ts";
 import { Application } from 'https://deno.land/x/oak@v10.6.0/mod.ts';
 
 const TELEGRAM_BOT_TOKEN = Deno.env.get("TELEGRAM_BOT_TOKEN")!;
 const TELEGRAM_CHAT_ID = Deno.env.get("TELEGRAM_CHAT_ID")!;
 const TELEGRAM_BOT_SECRET = TELEGRAM_BOT_TOKEN.replace(':', '');
+const WELCOME_MESSAGE = Deno.env.get("WELCOME_MESSAGE") ?? 'Welcome 👋\nHow can I help?';
 const REPLY_MESSAGE = Deno.env.get("REPLY_MESSAGE") ?? 'Reply to this message, please.';
+const WRONG_REPLY_MESSAGE = Deno.env.get("WRONG_REPLY_MESSAGE") ?? 'Reply to the proper message, please.';
 
 // Utils
 const userToString = (user?: User) =>
   !user ? '👻' : user.username ? `@${user.username}` : `${user.first_name} (${user.id})`;
 
+const retrieveUserId = (msg?: Message) => {
+  const forwardFrom = msg?.reply_to_message?.forward_from;
+  const replyToText = msg?.reply_to_message?.text;
+  return forwardFrom
+    ? forwardFrom.id
+    : replyToText?.includes(REPLY_MESSAGE) ? replyToText?.split('\n')?.[0] : null;
+}
+
 // Handlers
 const startHandler = async (ctx: Context) => {
-  await ctx.reply('Welcome 👋\nHow can I help?');
+  await ctx.reply(WELCOME_MESSAGE);
   await ctx.api.sendMessage(TELEGRAM_CHAT_ID, `👋 ${userToString(ctx.from)}`);
 }
 
@@ -24,10 +34,19 @@ const forwardToChat = async (ctx: Context) => {
   }
 }
 
+const forwardToUser = async (ctx: Context) => {
+  const userId = retrieveUserId(ctx.msg);
+  return userId
+    ? await ctx.api.copyMessage(userId, ctx.msg!.chat.id, ctx.msg!.message_id)
+    : await ctx.api.sendMessage(TELEGRAM_CHAT_ID, WRONG_REPLY_MESSAGE);
+}
+
 // Setup bot
 const bot = new Bot(TELEGRAM_BOT_TOKEN);
 bot.command("start", startHandler);
 bot.filter(ctx => ctx.chat?.type === 'private').on("message", forwardToChat);
+bot.filter(ctx => ctx.chat?.id?.toString() === TELEGRAM_CHAT_ID && !!ctx.msg?.reply_to_message)
+  .on("message", forwardToUser);
 
 // Setup oak
 const app = new Application();
