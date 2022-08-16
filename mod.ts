828c443828ceef92f21429be281fa87d0640a7d2
Sergii Golyshev <cyber.tchief@gmail.com>
2022-08-16 21:12:00 +0300

    feat: setup basic telegram bot with oak

---
+++ mod.ts
@@ -0,0 +1,18 @@
+import { Bot, Context, webhookCallback } from 'https://deno.land/x/grammy@v1.10.1/mod.ts';
+import { Application } from 'https://deno.land/x/oak@v10.6.0/mod.ts';
+
+const TELEGRAM_BOT_TOKEN = Deno.env.get("TELEGRAM_BOT_TOKEN")!;
+
+// Handlers
+const startHandler = async (ctx: Context) => {
+  await ctx.reply('Welcome 👋\nHow can I help?');
+}
+
+// Setup bot
+const bot = new Bot(TELEGRAM_BOT_TOKEN);
+bot.command("start", startHandler);
+
+// Setup oak
+const app = new Application();
+app.use(webhookCallback(bot, 'oak'));
+app.listen();
