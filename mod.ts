6d9ae6e379e68d8bd251ee287f49d30abf9cd381
Sergii Golyshev <cyber.tchief@gmail.com>
2022-08-16 21:30:21 +0300

    feat: forward private messages to support chat

--- mod.ts
+++ mod.ts
@@ -4,6 +4,7 @@ import { Application } from 'https://deno.land/x/oak@v10.6.0/mod.ts';
 const TELEGRAM_BOT_TOKEN = Deno.env.get("TELEGRAM_BOT_TOKEN")!;
 const TELEGRAM_CHAT_ID = Deno.env.get("TELEGRAM_CHAT_ID")!;
 const TELEGRAM_BOT_SECRET = TELEGRAM_BOT_TOKEN.replace(':', '');
+const REPLY_MESSAGE = Deno.env.get("REPLY_MESSAGE") ?? 'Reply to this message, please.';
 
 // Handlers
 const startHandler = async (ctx: Context) => {
@@ -11,9 +12,17 @@ const startHandler = async (ctx: Context) => {
   await ctx.api.sendMessage(TELEGRAM_CHAT_ID, `👋 ${JSON.stringify(ctx.from)}`);
 }
 
+const forwardToChat = async (ctx: Context) => {
+  const forwarded = await ctx.forwardMessage(TELEGRAM_CHAT_ID);
+  if (!forwarded.forward_from) {
+    await ctx.api.sendMessage(TELEGRAM_CHAT_ID, `${ctx.from?.id}\n${REPLY_MESSAGE}`);
+  }
+}
+
 // Setup bot
 const bot = new Bot(TELEGRAM_BOT_TOKEN);
 bot.command("start", startHandler);
+bot.filter(ctx => ctx.chat?.type === 'private').on("message", forwardToChat);
 
 // Setup oak
 const app = new Application();
